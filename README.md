0d0f729073eb74e163133d6326a4d79f78b2d5e6
Thiam Hock Ng <nthock@gmail.com>
2020-02-10 22:48:23 +0800

    update startup script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sample Terraform Code for GCP
+
+## Setting up
+
+Create a service account in your GCP, and save the file as `gcp_credentials.json`.
+
+```bash
+terraform init
+
+terraform plan
+
+terraform apply
+```
--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install apache2 -y
 
-echo '<!doctype html><html><body><h1>Hello World!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello World from Terraform!</h1></body></html>' | sudo tee /var/www/html/index.html
